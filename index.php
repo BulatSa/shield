cdfd35d1a07045c14999622c26717170e3079439
Andrew <andreiduffy@gmail.com>
2018-08-02 10:44:21 +0300

    panel triggers

--- index.php
+++ index.php
@@ -22,4 +22,108 @@
 	</div>
 </section>
 
+
+<section class="s-panel-section">
+	<div class="container">
+		<div class="row-big">
+			<div class="grid-4 grid-12_s">
+				<div class="panel">
+					<img src="/img/home/panel-1.jpg" alt="1" class="panel__img">
+					<div class="panel__content">
+						<div class="h2"><small>Более</small>100.000.000 <span class="rub-small">c</span></div>
+						<p class="panel__text">Вернули своим клиентам за 2016 год. Это 91% успешных сделок.</p>
+						<a href="#" class="link">Проверьте своего брокера</a>
+					</div>
+				</div>
+			</div>
+			<div class="grid-4 grid-12_s">
+				<div class="panel">
+					<img src="/img/home/panel-2.jpg" alt="1" class="panel__img">
+					<div class="panel__content">
+						<div class="h2"><small>Комиссия</small>~5%</div>
+						<p class="panel__text">Рассчитываем стоимость услуг от суммы потерянного депозита.</p>
+						<a href="#" class="link">Подробнее о тарифах</a>
+					</div>
+				</div>
+			</div>
+			<div class="grid-4 grid-12_s">
+				<div class="panel">
+					<img src="/img/home/panel-3.jpg" alt="1" class="panel__img">
+					<div class="panel__content">
+						<div class="h2"><small>Свыше</small>1.200 клиентов </div>
+						<p class="panel__text">Получили деньги назад за 5 лет работы компании в России и СНГ.</p>
+						<a href="#" class="link">Больше о компании</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="s-home-video">
+	<div class="container">
+		<div class="row">
+			<div class="grid-5 grid-6_xl grid-12_s">
+				<div class="h1">Не все потеряно!</div>
+				<div class="h3">Даже если брокер оказался мошенником</div>
+				<br>
+				<div class="text-big">С мошенничеством брокеров мы столкнулись лично и знаем, как это — потерять все деньги. Многие из наших клиентов — люди со средним достатком, которые просто хотели заработать на образование, квартиру, достойную старость, и за этим обратились к брокерам. К сожалению, не все из них надежны и добросовестны. Благодаря программе Chargeback украденные деньги можно вернуть. Для этого необходимо доказать, что брокер присвоил их себе, нарушив условия договора.</div>
+				<div class="info-block">Мы предлагаем свою помощь, чтобы грамотно провести процедуру и гарантированно вернуть ваши средства.</div>
+			</div>
+			<div class="grid-7 grid-6_xl grid-12_s">
+				<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="video-man fancy-video" rel="nofollow">
+					<img src="/img/home/video-man.jpg" alt="man" class="video-man__img">
+					<svg class="video-man__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 77.14 92.84">
+						<path class="cls-1" d="M75.54,49.63c2.13-1.3,2.13-5.11,0-6.42L6.89,1.33C1.53-1.94,0,1.62,0,3.56V89.28c0,1.94,1.53,5.5,6.89,2.23L75.54,49.63Z"/>
+					</svg>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="s-triggers def-sec">
+	<div class="container">
+		<div class="trigger-row trigger-row--5">
+			<div class="trigger">
+				<div class="trigger__head">
+					<img src="/img/triggers/1.png" alt="1" class="trigger__img--1">
+				</div>
+				<strong>Командная работа</strong>
+				<div class="text-small">Над каждым случаем работает опытная команда юристов, которые уже выигрывала дела именно у вашего брокера.</div>
+			</div>
+			<div class="trigger">
+				<div class="trigger__head">
+					<img src="/img/triggers/2.png" alt="1" class="trigger__img--2">
+				</div>
+				<strong>Консультации 24/7</strong>
+				<div class="text-small">Мы всегда на связи по телефону, почте, в социальных сетях и мессенджерах: Facebook, Telegram, Viber, WhatsApp.</div>
+			</div>
+			<div class="trigger">
+				<div class="trigger__head">
+					<img src="/img/triggers/3.png" alt="1" class="trigger__img--3">
+				</div>
+				<strong>Соблюдение правил и&nbsp;регламентов</strong>
+				<div class="text-small">Не действуем в обход закону и пытаемся сразу минимизировать вероятность повторного обращения в банк.</div>
+			</div>
+			<div class="trigger">
+				<div class="trigger__head">
+					<img src="/img/triggers/4.png" alt="1" class="trigger__img--4">
+				</div>
+				<strong>100% прозрачность</strong>
+				<div class="text-small">Вы всегда знаете, на каком этапе находится ваше дело, получая информацию от юристов в  кабинете системы и через личные сообщения.</div>
+			</div>
+			<div class="trigger">
+				<div class="trigger__head">
+					<img src="/img/triggers/5.png" alt="1" class="trigger__img--5">
+				</div>
+				<strong>Гарантия качества</strong>
+				<div class="text-small">Если в течение 10 дней вас не устроит качество наших услуг, мы полностью вернем вам деньги.</div>
+			</div>
+		</div>
+	</div>
+</section>
+
 <?php include('footer.php'); ?>
