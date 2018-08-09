ef0e90b8ed2a26961a496647ca6349cb1a8d0f48
Andrew <andreiduffy@gmail.com>
2018-08-09 10:06:18 +0300

    begin check

---
+++ broker-check.php
@@ -0,0 +1,26 @@
+﻿<?php include('header.php'); ?>
+
+<div class="s-head-simple">
+	<div class="container">
+		<div class="row">
+
+			<div class="grid-6">
+				<h1 class="h1">Насколько надежен ваш брокер?</h1>
+				<div class="h3">Проверьте его в нашем сервисе</div>
+				<div class="user-content">
+					<p class="text-big">Защищая интересы своих клиентов, мы собрали большую базу недобросовестных брокеров.</p>
+					<p class="text-big">Если у вас есть сомнения в честности вашего финансового посредника, проверьте его с помощью нашего сервиса. Это не займет много времени.</p>
+
+					<div class="broker-check">
+
+					</div>
+				</div>
+			</div>
+
+			<div class="grid-6"></div>
+
+		</div>
+	</div>
+</div>
+
+<?php include('footer.php'); ?>
