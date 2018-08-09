ed1bcd893234776df570d9febb33dd9d7f14a514
Andrew <andreiduffy@gmail.com>
2018-08-09 11:20:16 +0300

    404.php

---
+++ 404.php
@@ -0,0 +1,16 @@
+﻿<?php include('header.php'); ?>
+
+<div class="s-head-simple s-404">
+	<div class="container">
+		<h1 class="h1">Ошибка 404</h1>
+		<div class="h3">Еще не все потеряно - <span class="blue">Попробуйте запустить chargeback</span></div>
+
+		<div class="s-404__links">
+			<a href="/" class="link">Вернитесь на главную</a>
+			<a href="/broker-check.php" class="link">Проверьте своего брокера</a>
+			<a href="/about.php" class="link">Узнайте больше о нас</a>
+		</div>
+	</div>
+</div>
+
+<?php include('footer.php'); ?>
