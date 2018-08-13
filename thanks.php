312147f66934c539ad09460496d79d44a42050aa
Andrew <andreiduffy@gmail.com>
2018-08-13 09:08:05 +0300

    thanks ready

---
+++ thanks.php
@@ -0,0 +1,17 @@
+﻿<?php include('header.php'); ?>
+
+
+<section class="big-head-sec big-head-sec--thanks" style="background-image: url('../img/thanks/head-bg.jpg')">
+	<div class="container">
+		<div class="big-head">
+			<div class="big-head__info">
+				<h1 class="h1">Спасибо, <small>что обратились к нам!</small></h1>
+				<p>Мы свяжемся с вами в течение 15 минут и ответим на все интересующие вопросы.</p>
+				<div class="info-block">Вы можете самостоятельно проверить надежность своего брокера с помощью нашего сервиса.</div>
+				<a href="/broker-check.php" class="btn">Начать проверку брокера</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include('footer.php'); ?>
